e045d643cfb677599667e4824af692088f93f055
Marc Juul Christoffersen <marcjc@gmail.com>
2011-03-08 14:12:53 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dnand::Application.config.secret_token = '8d652b6d55de959b8665d092deed6684680e8b55ca4f0cccd298e8854f68d998354c32781977a977cfb57b0de75836139283d5149ea1e146f7c4129537bcd2e5'
